15b35261f4e236314c5ec2523dd12821295ec42c
Mary Angel <201910994@fit.edu.ph>
2024-03-22 14:09:14 +0800

    hero section

--- src/pages/sections/Hero.tsx
+++ src/pages/sections/Hero.tsx
@@ -1,16 +1,46 @@
+import { useEffect, useState } from "react";
+import { portfolioData } from "../../assets/portfolioData";
 
+const Hero = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-const  Hero = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const calculateTranslate = () => {
+    const maxScroll = document.body.clientHeight - window.innerHeight;
+    const translationRange = 50; // Range of translation in pixels
+    const translate = (scrollPosition / maxScroll) * translationRange;
+    return 50 - translate;
+  };
+
   return (
-    <div className="flex items-center justify-center w-full min-h-screen text-brown-100">
-        <div className="font-bold font-firaSans text-9xl">
-            MARY ANGEL SANDOVAL
+    <div className="flex items-center justify-center w-full gap-10 h-[100vw] text-brown-100 text-nowrap overflow-x-hidden"
+        style={{
+            transform: `translateX(${calculateTranslate()}%)`,
+        }}
+    >
+      <div className="font-bold font-firaSans text-9xl">
+        MARY ANGEL SANDOVAL
+      </div>
+      <div className="flex flex-col w-2/5 gap-2">
+        <div className="text-4xl font-medium">{portfolioData.hero.title}</div>
+        <div className="text-2xl text-pretty">
+          {portfolioData.hero.titleDescription}
         </div>
-        <div>
-            <div></div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export { Hero }
+export { Hero };
